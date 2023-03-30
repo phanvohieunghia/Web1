6ad3412938ed689fdd0e0efa8475dcc42b94b5f3
phanvohieunghia <phvohieunghia@gmail.com>
2023-03-30 23:48:52 +0700

    feat: finish bt7

---
+++ baitap7/main.js
@@ -0,0 +1,18 @@
+const resultElement = document.getElementById("result");
+let number = 240;
+
+function render(number) {
+  const x = `
+  <div class="col-lg-2 col-md-3 col-sm-4 col-6">
+  <img src="https://loremflickr.com/320/${number}" alt="image" />
+  </div>
+  `;
+  return x;
+}
+
+function autoIncrease() {
+  number++;
+  const x = render(number);
+  const container = x + resultElement.innerHTML;
+  resultElement.innerHTML = container;
+}
